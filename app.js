91dfb96cd88359680be705826b4638ead5d85e34
RooM0067 <chehjunhong55@gmail.com>
2025-05-21 23:11:43 +0800

    Change Lighting colour

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ class App{
 		this.scene = new THREE.Scene();
         this.scene.add( this.dolly );
         
-        const ambient = new THREE.HemisphereLight(0x1e1b40, 0x1e1b40, 0.3);
+        const ambient = new THREE.HemisphereLight(0x0c0b18, 0x0c0b18, 0.1);
 		this.scene.add(ambient);
 
 		this.renderer = new THREE.WebGLRenderer({ antialias: true });
